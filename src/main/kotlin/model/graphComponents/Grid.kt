b9579016b7828c2f393926c12ed5ef28c5aaa811
jack-brady <jackbradyx@proton.me>
2026-06-28 14:00:08 +1000

    Wire graph initializer interface to grid

--- src/main/kotlin/model/graphComponents/Grid.kt
+++ src/main/kotlin/model/graphComponents/Grid.kt
@@ -1,20 +1,18 @@
 package model.graphComponents
 
+import model.graphInitializers.GraphInitializer
+
 /** An NxN matrix of cells. Cells are initialized with border wall flags to designate boundaries. A collection of edges
  * map cells to their traversable neighbors. */
 data class Grid(
     val width: Int,
     val height: Int,
-    val cells: List<Cell> = List(width * height) { i ->
-        val x = i % height
-        val y = i / height
-        val isWall = x == 0 || x == width - 1 || y == 0 || y == height - 1
-        Cell(x, y, isWall)
-    },
-    val edges: Map<Cell, List<Edge>> = mutableMapOf()
+    val graphInitializer: GraphInitializer,
+    var cells: List<Cell> = mutableListOf(),
+    var edges: Map<Cell, List<Edge>> = mutableMapOf()
 ) {
 
     init {
-        require(width > 0 && height > 0) { "Width and height must be positive" }
+        cells = graphInitializer.initializeCells(width, height)
     }
 }
