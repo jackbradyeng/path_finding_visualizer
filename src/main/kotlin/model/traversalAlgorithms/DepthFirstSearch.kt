29e2b9f0682868135bf21cb99a60d720f1298f00
jack-brady <jackbradyx@proton.me>
2026-07-01 16:28:05 +1000

    Add termination condition to DFS

--- src/main/kotlin/model/traversalAlgorithms/DepthFirstSearch.kt
+++ src/main/kotlin/model/traversalAlgorithms/DepthFirstSearch.kt
@@ -6,8 +6,10 @@ import model.graphComponents.Grid
 
 class DepthFirstSearch : TraversalAlgorithm {
 
+    /** Traverses a given graph sequentially, leveraging the stack's LIFO behaviour to achieve DFS. */
     override fun traverse(cell: Cell, grid: Grid, visited: List<Cell>): List<Cell> {
         val visitedMutable = visited.toMutableList()
+
         // non-recursive DFS requires a stack (LIFO behaviour) for correct functionality
         val stack: ArrayDeque<Cell> = ArrayDeque()
 
@@ -15,10 +17,16 @@ class DepthFirstSearch : TraversalAlgorithm {
 
         while (stack.isNotEmpty()) {
             val current = stack.removeLast()
+
             // skip the current cell if it has already been visited
             if (current in visitedMutable) continue
-            // node is pushed onto the stack only when it is visited
+
+            // push the current node onto the stack only when it has been visited
             visitedMutable.add(current)
+
+            // if the current cell is the target cell, then break out of the loop
+            if (current == grid.end) return visitedMutable
+
             val edges: List<Edge> = grid.edges[current] ?: emptyList()
 
             for (edge in edges) {
